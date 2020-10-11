91efb0dbdb61be53042e1e33e94348b219dba3ca
Mikael Agabalyants <njxqlus@gmail.com>
2020-10-11 23:02:41 +0300

    refactor(problem_1): tests

--- tests/problem_1.py
+++ tests/problem_1.py
@@ -3,17 +3,17 @@ import time
 from euler.problems.problem_1 import solve
 
 
-class Problem1TestCase(unittest.TestCase):
+class SolutionTestCase(unittest.TestCase):
     condition = 1000
     answer = 233168
 
-    def test_default_condition(self):
+    def test_solution_with_default_condition(self):
         self.assertEqual(solve(10), 23)
 
-    def test_target_condition(self):
+    def test_solution(self):
         self.assertEqual(solve(self.condition), self.answer)
 
-    def test_time(self):
+    def test_solution_time(self):
         start_time = time.time()
         solve(self.condition)
         self.assertLess(time.time() - start_time, 60)
